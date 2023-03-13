cc90ecd57b9a6f1b99ddb95f32a52229b5fce510
ScienceOwl13 <groencg@gmail.com>
2023-03-13 16:23:03 -0400

    Add the proper buttons!

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 // Got from https://www.w3schools.com/js/tryit.asp?filename=tryjs_function_counter3
 
-var addPlayer1 = (function () {
-    var counter = 0;
-    return function () {return counter += 1;}
-})();
 
-function player1Function(){
-    document.getElementById("demo").innerHTML = addPlayer1();
-}
 
-var addPlayer2 = (function () {
-    var counter = 0;
-    return function () {return counter += 1;}
-})();
+var p1counter = 0;
 
-function player2Function(){
-    document.getElementById("demo2").innerHTML = addPlayer2();
-}
+    function player1AddFunction(){
+        p1counter += 1;
+        document.getElementById("demo1").innerHTML = p1counter;
+    }
+    function player1SubFunction(){
+        p1counter -= 1;
+        document.getElementById("demo1").innerHTML = p1counter;
+    }
+
+
+//Player 2
+
+var p2counter = 0;
+
+    function player2AddFunction(){
+        p2counter += 1;
+        document.getElementById("demo2").innerHTML = p2counter;
+    }
+
+    function player2SubFunction(){
+        p2counter -= 1;
+        document.getElementById("demo2").innerHTML = p2counter;
+    }
